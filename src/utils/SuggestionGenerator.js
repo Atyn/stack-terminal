fec830c8b5e790f45a5790ccd7e25d47f490c31c
Anton <atynatyn@gmail.com>
2019-02-18 20:21:09 +0100

    Added git and npm autocompletions

--- src/utils/SuggestionGenerator.js
+++ src/utils/SuggestionGenerator.js
@@ -6,8 +6,58 @@ export default {
 		switch (command) {
 			case 'cd ':
 				return await this.getDirectoryList(cwd)
+			case 'npm ':
+				return await this.getNpmCommandList(cwd)
+			case 'npm uninstall ':
+				return await this.getNpmUninstallList(cwd)
+			case 'git ':
+				return await this.getGitCommands(cwd)
 		}
 		return null
+	},
+	async getGitCommands() {
+		return [
+			'pull',
+			'checkout',
+			'status',
+			'push',
+			'commit',
+		]
+	},
+	async getNpmUninstallList(cwd) {
+		try {
+			const packageJson = await FsExtra.readFile(
+				Path.resolve(cwd, 'package.json')
+			)
+			const obj = JSON.parse(packageJson.toString())
+			const map = Object.assign({}, obj.dependencies, obj.devDependencies)
+			return Object.keys(map)
+		} catch (error) {
+			return []
+		}
+	},
+	async getNpmCommandList(cwd) {
+		const commandList = [
+			'start',
+			'install',
+			'uninstall',
+		]
+		try {
+			const packageJson = await FsExtra.readFile(
+				Path.resolve(cwd, 'package.json')
+			)
+			const obj = JSON.parse(packageJson.toString())
+			return [
+				...commandList,
+				...Object.keys(obj.scripts)
+					.map(scriptName => ['run', scriptName].join(' ')),
+			]
+		} catch (error) {
+			return [
+				...commandList,
+				'init',
+			]
+		}
 	},
 	async getDirectoryList(cwd) {
 		const list = await FsExtra.readdirSync(cwd)
