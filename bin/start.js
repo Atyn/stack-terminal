dee1796ea4e7ca83ffbda725e919c0654c78797b
Anton <atynatyn@gmail.com>
2019-02-25 16:10:28 +0100

    Run by installing over npm has been fixed!

--- bin/start.js
+++ bin/start.js
@@ -4,9 +4,11 @@ const electronPath = require('electron')
 const ChildProcess = require('child_process')
 const Path = require('path')
 
-const command = [
-	electronPath,
-	Path.resolve(__dirname, 'electron-apps', 'production'),
-].join(' ')
-
-ChildProcess.exec(command, { stdout: 'pipe' })
+ChildProcess.spawn(
+	electronPath, [Path.resolve(__dirname, '../electron-apps', 'production'),
+	], {
+		// detached: true,
+		stdout: 'pipe',
+		stdin:  'pipe',
+		stderr: 'pipe',
+	})
