dcd414f8f9ddca96a6047fc15edd1a1529bc446b
Anton <atynatyn@gmail.com>
2019-02-26 18:43:17 +0100

    It can now take a path argument when starting

--- bin/start.js
+++ bin/start.js
@@ -4,9 +4,15 @@ const electronPath = require('electron')
 const ChildProcess = require('child_process')
 const Path = require('path')
 
+const path = process.argv[2]
+const cwd = path ?
+	Path.resolve(process.cwd(), path) :
+	process.cwd()
+
 ChildProcess.spawn(
 	electronPath, [Path.resolve(__dirname, '../electron-apps', 'production'),
 	], {
+		cwd,
 		// detached: true,
 		stdout: 'pipe',
 		stdin:  'pipe',
