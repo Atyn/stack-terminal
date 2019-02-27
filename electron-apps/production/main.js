540397f1e4ae8cf2169fbd41af3b9b6ae023e7d9
Anton <atynatyn@gmail.com>
2019-02-27 18:41:27 +0100

    Added icon

--- electron-apps/production/main.js
+++ electron-apps/production/main.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require('electron')
+const Path = require('path')
 
 function createWindow() {
-	const win = new BrowserWindow({ width: 800, height: 600 })
+	const win = new BrowserWindow({ 
+		icon:   Path.resolve(__dirname, '../../icon.svg'),
+		width:  800, 
+		height: 600, 
+	})
 	win.setMenu(null)
 	win.setAutoHideMenuBar(true)
 	win.loadFile('index.html')
