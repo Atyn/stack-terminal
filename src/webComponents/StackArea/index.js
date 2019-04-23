fab0cc5f5f6afd7f040d3a6278fdd198d572cc7b
Anton <atynatyn@gmail.com>
2019-04-23 21:37:19 +0200

    Added scroll down button

--- src/webComponents/StackArea/index.js
+++ src/webComponents/StackArea/index.js
@@ -3,6 +3,15 @@ import Debouncer from '../../utils/Debouncer'
 import FsExtra from 'fs-extra'
 
 const tagName = 'terminal-stack-area'
+const templates = {
+	scrollDownButton: document.createElement('div'),
+}
+templates.scrollDownButton.innerHTML = `
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" fill="currentColor"/>
+		<path fill="none" d="M0 0h24v24H0V0z"/>
+	</svg>
+`
 const hostStyle = {
 	overflowY: 'hidden',
 	overflowX: 'hidden',
@@ -10,10 +19,7 @@ const hostStyle = {
 	position: 'relative',
 	// 'scroll-behavior': 'smooth',
 }
-const mutationObserverConfig = {
-	childList: true,
-	subtree: true,
-}
+const defaultScrollButtonOpacity = 0.8
 const lockStateStyle = {
 	position: 'absolute',
 	right: '10px',
@@ -27,17 +33,41 @@ const scrollContainerStyle = {
 	flexDirection: 'column',
 }
 
+Object.assign(templates.scrollDownButton.style, {
+	position: 'absolute',
+	backgroundColor: 'black',
+	color: 'white',
+	borderRadius: '50%',
+	left: '47%',
+	opacity: defaultScrollButtonOpacity,
+	padding: 'var(--default-margin)',
+	bottom: 'var(--default-margin)',
+	transition: 'opacity 0.6s',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	cursor: 'pointer',
+})
+
 export default tagName
 
 class WebComponent extends HTMLElement {
 	constructor() {
 		super()
 		this.listener = this.onFileChanged.bind(this)
+		this.elements = {
+			scrollDownButton: templates.scrollDownButton.cloneNode(true),
+		}
 		const shadowRoot = this.attachShadow({ mode: 'open' })
 		shadowRoot.innerHTML = ''
 		this.scrollContainer = document.createElement('div')
+		this.elements.scrollDownButton.addEventListener(
+			'click',
+			this.scrollToBottom.bind(this)
+		)
 		Object.assign(this.scrollContainer.style, scrollContainerStyle)
 		shadowRoot.appendChild(this.scrollContainer)
+		shadowRoot.appendChild(this.elements.scrollDownButton)
 	}
 	onFileChanged(changeType, filename) {
 		this.list.push(filename) // Keep list in sync
@@ -59,8 +89,12 @@ class WebComponent extends HTMLElement {
 	onDebouncedScroll() {
 		if (this.scrollIsDown()) {
 			this.lockedScrollState = true
+			this.elements.scrollDownButton.style.opacity = 0
+			this.elements.scrollDownButton.style.pointerEvents = 'none'
 		} else {
 			this.lockedScrollState = false
+			this.elements.scrollDownButton.style.opacity = defaultScrollButtonOpacity
+			this.elements.scrollDownButton.style.pointerEvents = null
 		}
 	}
 	async connectedCallback() {
@@ -83,6 +117,7 @@ class WebComponent extends HTMLElement {
 		)
 		setTimeout(() => this.scrollToBottom(), 200)
 	}
+	getScrollDownButton() {}
 	onContentAdded() {
 		if (this.lockedScrollState) {
 			this.scrollToBottom()
